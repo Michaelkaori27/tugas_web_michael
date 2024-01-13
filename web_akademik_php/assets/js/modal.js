3f6a7d2f9bf6c4db3e7079e3bb47fb80f86f0e1f
Michaelkaori27 <156342457+Michaelkaori27@users.noreply.github.com>
2024-01-13 17:01:53 +0700

    Add files via upload

--- web_akademik_php/assets/js/modal.js
+++ web_akademik_php/assets/js/modal.js
@@ -1,27 +1,25 @@
-@import url("https://fonts.googleapis.com/css?family=Lato:400,700&display=swap");
-/* fungsi dari kode diatas adalah mengimpor gaya font "Lato" dari Google Fonts dengan bobot 400 dan 700, dan 
-  menetapkan tampilan yang optimal untuk meningkatkan kinerja tampilan teks di halaman web. */
 
 function show_modal(iteration){
-    /* Dapatkan modal */
-    var modal = document.getElementById("myModal"+iteration);
+	// Get the modal
+	var modal = document.getElementById("myModal"+iteration);
 
-    /* Dapatkan elemen <span> yang menutup modal */
-    var span = document.getElementById("close"+iteration);
+	// Get the <span> element that closes the modal
+	var span = document.getElementById("close"+iteration);
 
-    /* Ketika pengguna mengklik tombol, buka modal*/
-    btn.onclick = function() {
-      modal.style.display ="block";
-    }
-    /*Ketika pengguna mengklik <span> (x), tutup modal */
-    span.onclick = function() {
-      modal.style.display="none";
-    }
+	// When the user clicks the button, open the modal 
+	//btn.onclick = function() {
+	modal.style.display = "block";
+	//
 
-    /*Ketika pengguna mengklik di luar modal, tutup modal */
-    window.onclick = function (event) {
-        if (event.target == modal) { 
-           modal.style.display = "none";
-        }
-    }
+	// When the user clicks on <span> (x), close the modal
+	span.onclick = function() {
+	  modal.style.display = "none";
+	}
+
+	// When the user clicks anywhere outside of the modal, close it
+	window.onclick = function(event) {
+	  if (event.target == modal) {
+	    modal.style.display = "none";
+	  }
+	}
 }
